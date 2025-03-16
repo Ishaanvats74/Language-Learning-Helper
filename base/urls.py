49a4cff20d65debf776d3bb86935b8a5f2d845ad
Ishaanvats74 <Ishaanvats74@gmail.com>
2025-03-17 00:39:36 +0530

    completed till 1:02:00

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.Home , name="Home"),
-    path('About_us/',views.About_us , name="About us"),
+    path('About_us/<str:pk>/',views.About_us , name="About us"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse 
+details = [
+    {'id':1 , 'name' : 'Lets! learn Python'},
+    {'id':2 , 'name' : 'Lets! learn JavaScript'},
+    {'id':3 , 'name' : 'Lets! learn Css'},
+]
+
 
 def Home(request):
-    return HttpResponse('Home page')
+    context = {'details':details}
+    return render(request,'base/home.html' , context)
 
-def About_us(request):
-    return HttpResponse('About Us Page')
+def About_us(request,pk):
+    About_us = None
+    for i in details:
+        if i['id'] == int(pk):
+            About_us = i 
+    context = {'About_us' : About_us}
+    return render(request,'base/about_us.html',context)
